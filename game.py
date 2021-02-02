0e851b4ca7f7a9a38733cab18e5b4ffd45db5611
LeeYee1125 <75297957+LeeYee1125@users.noreply.github.com>
2021-02-02 16:13:38 +0800

    Add files via upload

--- game.py
+++ game.py
@@ -1,48 +1,88 @@
-from tkinter import*
-win = Tk()  #製作按鈕
-win.title("請問是否要開始遊戲?")
-win.geometry("400x200")
+from tkinter import*#載入模組
+import random as rd
+from tkinter import messagebox
 
-#def game():
-    #import random   #1A2B
-#x=random.sample('1234567890',4)
-#i=0
-#j=0
-#t=0
-#while play:
-    #y = input("請輸入你猜的4位數字:")
-    #z = list(y)
-    #if x == z:
-      #t=t+1
-      #print("4A0B","共猜",t,"次") 
-      #break
-      #win.mainloop()    
-    #else: 
-      #a=0
-      #b=0     
-      #t=t+1
-      #for i in range(4):
-        #if x[i] == z[i]:
-          #a=a+1
-      #for i in range(4):
-       # for j in range(4):
-          #if i == j:
-            #pass
-          #else:
-            #if x[i] == z[j]:
-              #b = b + 1            
-      #print(a,"A",b,"B")     
-      #continue
+def gameover():#結束遊戲內容
+      MsgBox = messagebox.askquestion ('結束遊戲','是否繼續退出?',icon = 'error')
+      if MsgBox == 'yes':
+          win1.destroy()
+      else:
+          messagebox.showinfo('歡迎','歡迎回來')
 
-#def off():
-      #print("謝謝，歡迎再來玩")
+def game():#遊戲介面
+    win2 = Tk()#視窗
+    win2.title("開始遊戲")
+    win2.geometry("400x200")
 
-btn1 = Button(text="開始遊戲",bg="sky blue")
-#btn1.config(command = game)
-btn1.pack()
+    guessLabel = Label(win2,text='請輸入一組2位數')#玩家輸入的地方
+    guessnumber = Entry(win2)
+    guessLabel.pack()
+    guessnumber.pack() 
+    
+    anser = rd.randint(10,99)#定義變數
 
-btn2 = Button(text="結束遊戲",bg="red")
-#btn2.config(commang = off)
-btn2.pack()
+    def enter():#輸入鍵內容
+        num = 0
+        play = True
+        while True:#遊戲開始
+            guess = int(guessnumber.get())
+            if anser == guess:
+                num+=1
+                play = False
+                MsgBox = messagebox.askquestion ('獲勝','恭喜你贏得勝利，是否繼續下一局?',icon = 'warning')
+                if MsgBox == 'yes':
+                    reset()
+                else:
+                    win2.destroy()
+            elif anser < guess:
+                Label(win2,text = '太大了，再小一點!')
+                num+=1
+            else :
+                Label(win2,text = '太小了，再大一點!')
+                num+=1
 
-win.mainloop()
+        def numguess():#猜的次數
+            if num == 1 :
+                Label(win2,text = '好厲害，一次就答對了')
+            elif 1 < num < 20 :
+                Label(win2,text = '不錯喔，你很有實力!')
+            else :
+                Label(win2,text = '再加油吧，已經猜超過20次了!')
+
+    def reset():#重玩內容
+        global num
+        global running
+        anser = rd.randint(10,99)
+        num = 0
+        running = True
+        guessnumber.delete(0,'end')
+        resetlabel = messagebox.showinfo('重新開始','重新一局新遊戲')
+
+
+    def stopgame():#結束按鈕內容
+      MsgBox = messagebox.askquestion ('結束','重新進入後，會重新開始新遊戲，是否繼續退出?',icon = 'error')
+      if MsgBox == 'yes':
+          win2.destroy()
+      else:
+          messagebox.showinfo('歡迎','歡迎回來')
+
+    enterbutton = Button(win2,text = '確定',command = enter)#輸入按鈕
+    resetbutton = Button(win2,text = '重新一局',command = reset)#重玩按鈕
+    stopgamebtn = Button(win2,text = '結束',command = stopgame)#結束按鈕
+    enterbutton.pack()
+    resetbutton.pack()
+    stopgamebtn.pack()
+
+    win2.mainloop()
+
+win1 = Tk()  #開始畫面
+win1.title("歡迎來到猜數字遊戲")
+win1.geometry("400x200")
+
+startgamebutton = Button(win1,text="開始遊戲",bg="green",command = game)#開始遊戲按鈕
+startgamebutton.pack()
+
+gameoverbutton = Button(win1,text="結束遊戲",bg='red',command=gameover)#結束遊戲按鈕
+gameoverbutton.pack()
+
+win1.mainloop()
